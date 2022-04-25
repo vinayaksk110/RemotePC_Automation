a63450377a76d9c5df72a43aba9163b30ea4bb35
Vinayaksk110 <vinayaksk110@gmail.com>
2022-04-25 20:09:15 +0530

    merge checking

--- src/test/java/JavaPrograms/superclassOfJava.java
+++ src/test/java/JavaPrograms/superclassOfJava.java
@@ -4,6 +4,7 @@ public class superclassOfJava {
 	
 	public static void main(String[] args) {
 		
+		// this is to test the new line
 		superclassOfJava obj = new superclassOfJava();
 		Class cls = obj.getClass().getSuperclass();
 		System.out.println(cls.getName());

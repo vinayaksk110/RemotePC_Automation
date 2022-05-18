14a8a6723868e4f1d60e83b4848f85a80dabafec
Vinayaksk110 <vinayaksk110@gmail.com>
2022-05-18 21:25:17 +0530

    h

--- src/test/java/JavaPrograms/Pattern.java
+++ src/test/java/JavaPrograms/Pattern.java
@@ -4,12 +4,28 @@ public class Pattern {
 	
 	public static void main(String[] args) {
 		star1();
+		star2();
 	}
 	
 	public static void star1() {
+//		*****
+//		****
+//		***
+//		**
+//		*
 		int row=5;
 		for(int i=0;i<row;i++) {
 			for(int j=i;j<row;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void star2() {
+		int row=5;
+		for(int i=0;i<=row;i++) {
+			for(int j=0;j<=i;j++) {
 				System.out.print("*");
 			}
 			System.out.println();
